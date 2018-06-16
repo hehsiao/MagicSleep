6ffe9435f507a92cb44b055ff6081e92793fef96
Henry Hsiao <henry.hsiao@poweredbygrow.com>
2018-06-15 21:44:24 -0700

    Initial Commit

---
+++ MagicSleep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicSleep
+//
+//  Created by Henry Hsiao on 2018-06-15.
+//  Copyright © 2018 Henry Hsiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
